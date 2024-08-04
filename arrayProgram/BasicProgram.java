a0be6fca8df9122f121408b18093a0a4e244ad1c
yashodeepfasale <yashodeepfasale2512@gmail.com>
2024-08-04 22:42:16 +0530

    first commit

---
+++ arrayProgram/BasicProgram.java
@@ -0,0 +1,11 @@
+package arrayProgram;
+
+public class BasicProgram {
+
+    public void basicProgram(){
+
+    }
+    public static void main(String[] args) {
+        System.out.println("");
+    }
+}
